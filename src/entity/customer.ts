4b239bfcd12ec49c01e1fcdba36610bd61f21bee
Edio Gama <ediogama@hotmail.com>
2023-08-02 19:44:52 -0300

    add entity customer

---
+++ src/entity/customer.ts
@@ -0,0 +1,40 @@
+class Customer {
+
+    _id: string;
+    _name: string;
+    _address: string;
+    _IsActive: boolean = false;
+
+    constructor(id: string, name: string, address: string) {
+        this._id = id;
+        this._name = name;
+        this._address = address;
+        this.validate();
+    }
+
+    validate() {
+        if (this._name.length === 0) {
+            throw new Error('Customer name cannot be empty');
+        }
+
+        if (this._id.length === 0) {
+            throw new Error('Customer id cannot be empty');
+        }
+    }
+
+    changeName(name: string) {
+        this._name = name;
+        this.validate();
+    }
+
+    activate() {
+        if (this._address.length === 0) {
+            throw new Error('Customer address cannot be empty');
+        }
+        this._IsActive = true;
+    }
+
+    deactivate() {
+        this._IsActive = false;
+    }
+}
